63c8771fc88a9c11cdab071033851d7e39271646
Kaique Silva <kaique.developer@gmail.com>
2011-11-06 20:50:25 -0200

    Jsabstract
    - Mop function for repeat objects and return string

--- js/mop.js
+++ js/mop.js
@@ -1,13 +1,28 @@
 //Mop for no tipically operations
 function init() {
-	Function.prototype.mop = function () {
-		var args = arguments,
-			operators = ['*','+','-','/'];
+	String.prototype.mop = function () {
+		var capture = this,
+			clone = capture.toString();
+			args = arguments;
 		
-		return 'Funcionando!';
-	};
-	
-	mop = Function.mop;
+		if (capture !== '' && args !== 'undefined' && args[0] !== '') {
+			capture = capture.toString()
+			var i = 1;
+			
+			while(true) {
+				if (i < args[0]) {
+					i++;
+					capture += clone;
+				} else {
+					break;
+				}
+			}
+			
+			return capture;
+		} else {
+			return false;
+		}
+	}
 }
 
 (function () {
