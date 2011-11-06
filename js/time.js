3c8fd68e4f700075dccbf0799aa9746b147e7d0d
Kaique Silva <kaique.developer@gmail.com>
2011-11-06 00:54:26 -0200

    Jsabstract
    - Time ok

---
+++ js/time.js
@@ -0,0 +1,128 @@
+//Time function for use a time object inherited by Date object.
+function init(){
+	//Init object
+	Time = {};
+	
+	Function.prototype.time = function () {
+		return new Date();
+	};
+	
+	Time.time = Function.time();
+	
+	//Date: Week, Day, Month, Year
+	
+	Time.week = function (week) {
+		week = this.time.getDay();
+		return parseInt(week);
+	};
+	
+	Time.day = function (day) {
+		day = this.time.getDate();
+		return parseInt(day);
+	};
+	
+	Time.month = function (month) {
+		month = this.time.getMonth();
+		return parseInt(month);
+	};
+	
+	Time.year = function (year) {
+		if (typeof year !== 'undefined') {
+			if (year == 'short') {
+				year = this.time.getYear();
+				return parseInt(year);		
+			} else {
+				return false;
+			}
+		} else {
+			year = this.time.getFullYear();
+			return parseInt(year);
+		}
+	};
+	
+	//Time: Hour, Minuts, Seconds, Miliseconds
+	
+	Time.hour = function (hour) {
+		hour = this.time.getHours();
+		return parseInt(hour);
+	};
+	
+	Time.minute = function (minute) {
+		minute = this.time.getMinutes();
+		return parseInt(minute);
+	};
+	
+	Time.second = function (second) {
+		second = this.time.getSeconds();
+		return parseInt(second);
+	};
+	
+	Time.millisecond = function (milisecond) {
+		milisecond = this.time.getMilliseconds();
+		return parseInt(milisecond);
+	};
+	
+	//Exactly time and date
+	
+	Time.now_date = function () {
+		var args = arguments,
+			c = 0;
+
+		if (typeof args[0] !== 'undefined') {
+			while (true) {
+				if (c == 3) {
+					break;
+				} else {
+					if (args[0].search(/([%][d|D]){1}/) !== 1) {
+						args[0] = args[0].replace(/([%][d|D])/, this.day());
+					} if (args[0].search(/([%][m|M])/) !== 1) {
+						args[0] = args[0].replace(/([%][m|M])/, this.month());
+					} if (args[0].search(/([%][y|Y])/) !== 1) {
+						args[0] = args[0].replace(/([%][y|Y])/, this.year());
+					} else {
+						return false;
+					}
+				}
+			
+				c++;
+			}
+		
+			return args[0];	
+		} else {
+			return false;
+		}
+	};
+	
+	Time.now_time = function () {
+		var args = arguments,
+			c = 0;
+
+		if (typeof args[0] !== 'undefined') {
+			while (true) {
+				if (c == 3) {
+					break;
+				} else {
+					if (args[0].search(/([%][h|H]){1}/) !== 1) {
+						args[0] = args[0].replace(/([%][h|H])/, this.hour());
+					} if (args[0].search(/([%][m|M])/) !== 1) {
+						args[0] = args[0].replace(/([%][m|M])/, this.minute());
+					} if (args[0].search(/([%][s|S])/) !== 1) {
+						args[0] = args[0].replace(/([%][s|S])/, this.second());
+					} else {
+						return false;
+					}
+				}
+			
+				c++;
+			}
+		
+			return args[0];	
+		} else {
+			return false;
+		}	
+	};
+}
+
+(function () {
+	init()
+} ())
